cc93c0074a548733e8ae49a21dbd705792b85c41
Brinda George <brindageorge94@gmail.com>
2017-10-05 14:21:11 +0530

    controllers added

---
+++ InterviewEvaluationSystem/Controllers/InterviewerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewEvaluationSystem.Controllers
+{
+    public class InterviewerController : Controller
+    {
+        // GET: Interviewer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
