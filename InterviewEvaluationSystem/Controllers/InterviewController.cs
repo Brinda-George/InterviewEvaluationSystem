ab355213f452df4018e6ed85f1bb2e037698b0b4
Brinda George <brindageorge94@gmail.com>
2017-10-05 13:08:07 +0530

    Add project files.

---
+++ InterviewEvaluationSystem/Controllers/InterviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewEvaluationSystem.Controllers
+{
+    public class InterviewController : Controller
+    {
+        // GET: Interview
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InterviewEvaluationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InterviewEvaluationSystem.MvcApplication" Language="C#" %>
