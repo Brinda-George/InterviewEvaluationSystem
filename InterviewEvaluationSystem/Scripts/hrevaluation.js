b16d5ddf8261495a64dd719ee6b6bacb2c38e022
Brinda George <brindageorge94@gmail.com>
2017-11-09 14:42:23 +0530

    comments

--- InterviewEvaluationSystem/Scripts/hrevaluation.js
+++ InterviewEvaluationSystem/Scripts/hrevaluation.js
@@ -1,7 +1,13 @@
 ﻿$(document).ready(function () {
+
+    // Disable all drop downs
     $(".grid").find("select").attr("disabled", true);
+
+    // Get current round id
     var roundID = $('#roundId').val();
     var recommend = $('#recommended').val();
+
+    // Enable drop downs for current round
     if (recommend == "null") {
         $('.class' + roundID).attr("disabled", false);
     }
@@ -10,6 +16,8 @@
 function getValues(element) {
     var roundID = $('#roundId').val();
     var evaluationID = $('#evaluationId').val();
+
+    // Get id of button clicked
     var buttonclicked = element.id;
     if (buttonclicked == "proceed") {
         var recommended = true;
@@ -17,38 +25,53 @@ function getValues(element) {
     else {
         var recommended = false;
     }
-    var count = $('.class' + roundID).length;
+
     var valueArray = [];
     var idArray = [];
+
+    // Get skills of current round
     var skills = $('select[id^="id' + roundID + '"]');
     for (var i = 0; i < skills.length; i++) {
         var itemid = $(skills[i]).attr('id');
         var commonid = "id" + roundID;
+
+        // Check if all drop downs are selected
         if ($('#' + itemid).find('option:selected').val().length === 0) {
             alert("Please select all rates!!")
             return false;
         }
         else {
+            // Get id of particular skill
             idArray[i] = itemid.replace(commonid, '');
+
+            // Get rate scale value selected for particular skill
             valueArray[i] = $('#' + itemid).find('option:selected').val();
         }
     }
+
+    // Check if comment is given
     if ($('#Comments').val().length === 0) {
         alert("Please enter your comments!!")
         return false;
     }
-    else if ($('#Comments').val().length > 250) {
-        alert("The Comments field cannot contain more than 500 characters!!!")
+    // Check if comment length is greater than 1000 characters
+    else if ($('#Comments').val().length > 1000) {
+        alert("The Comments field cannot contain more than 1000 characters!!!")
         return false;
     }
     else {
+        // Get comments
         var comments = $('#Comments').val();
     }
+
+    // Submit values to post method to enter to db
     $.ajax({
         url: '/HR/HREvaluation',
         type: 'post',
         data: { recommended: recommended, evaluationID: evaluationID, ids: idArray, values: valueArray, comments: comments },
         success: function (response) {
+
+            // Redirect to response url
             window.location.href = response.Url;
         },
         error: function () {
@@ -56,6 +79,8 @@ function getValues(element) {
     });
 }
 
+
+// Print contents of div
 function printDiv() {
     $(".divContainer").print();
 }
